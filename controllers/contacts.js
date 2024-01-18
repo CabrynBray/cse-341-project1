f3507dee534e8f31b3dc4d0b9fdc88a5f1e850e8
CabrynBray <91556711+CabrynBray@users.noreply.github.com>
2024-01-18 17:04:55 +0200

    start of week two assignment

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,7 +1,7 @@
 const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async(req, res) => {
+const getAll = async (req, res) => {
     const result = await mongodb.getDatabase().db().collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
@@ -9,16 +9,29 @@ const getAll = async(req, res) => {
     });
 };
 
-const getSingle = async(req, res) => {
+const getSingle = async (req, res) => {
     const contactId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
+    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: contactId });
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
     });
 };
 
+const createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+};
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- server.js
+++ server.js
@@ -7,14 +7,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
+app.use(bodyParser.json());
 
 
 mongodb.initDb((err) => {
-    if(err) {
+    if (err) {
         console.log(err);
     }
     else {
-        app.listen(port, () => {console.log(`Database is listening and node Running on port ${port}`)});
+        app.listen(port, () => { console.log(`Database is listening and node Running on port ${port}`) });
     }
 });
 

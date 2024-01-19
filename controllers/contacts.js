a72aa693f3c6cd7261c304cb20e9529c553d3545
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2024-01-19 11:35:03 +0200

    instaled swagger

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async(req, res) => {
+    //#swagger.tags=['contacts']
     const result = await mongodb.getDatabase().db().collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
@@ -10,6 +11,7 @@ const getAll = async(req, res) => {
 };
 
 const getSingle = async(req, res) => {
+    //#swagger.tags=['contacts']
     const contactId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('contacts').find({_id: contactId});
     result.toArray().then((contacts) => {
@@ -18,7 +20,60 @@ const getSingle = async(req, res) => {
     });
 };
 
+const createUser = async (req, res) => {
+    //#swagger.tags=['contacts']
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(user);
+    if (response.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+    }
+};
+
+const updateUser = async (req, res) => {
+    //#swagger.tags=['contacts']
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        // username: req.body.username,
+        // email: req.body.email,
+        // name: req.body.name,
+        // ipaddress: req.body.ipaddress
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    };
+    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({_id: userId}, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occured while updating the user.');
+    }
+};
+
+const deleteUser = async (req, res) => {
+    //#swagger.tags=['contacts']
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({_id: userId});
+    if (response.deleteCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the user.');
+    }
+};
+
 module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    createUser,
+    updateUser,
+    deleteUser
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => { res.send('My Contacts')});
+router.use('/', require('./swagger'));
+
+router.get('/', (req, res) => { 
+    //#swagger.tags=['Hello World']
+    res.send('My Contacts');
+});
 
 router.use('/contacts', require('./contacts'));
 

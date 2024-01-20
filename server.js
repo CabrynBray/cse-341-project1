01cd32ea9c25c5934d8ed0b0dca8f6629dd39d83
CabrynBray <cabrynbrayza@gmail.comgit config --global user.name>
2024-01-20 19:00:59 +0200

    added to the rest

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongodb = require('./data/database');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 
